ed357b7dae3c3fa143117cd49ae1ed31b8774132
tmynoy <a6.n_h.x@icloud.com>
2020-11-09 19:53:55 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
     validates :nickname, length: { maximum: 40 }, uniqueness: { case_sensitive: true }
     NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
     NAME_CHECK_REGEX = /\A[ァ-ン]+\z/.freeze
-    validates :last_name, presence: true, format: { with: NAME_REGEX }
-    validates :first_name, presence: true, format: { with: NAME_REGEX }
-    validates :last_name_check, presence: true, format: { with: NAME_CHECK_REGEX }
-    validates :first_name_check, presence: true, format: { with: NAME_CHECK_REGEX }
-    validates :birthday, presence: true
+    validates :last_name, format: { with: NAME_REGEX }
+    validates :first_name, format: { with: NAME_REGEX }
+    validates :last_name_check, format: { with: NAME_CHECK_REGEX }
+    validates :first_name_check, format: { with: NAME_CHECK_REGEX }
+    validates :birthday
   end
   validates :email, uniqueness: 1, uniqueness: { case_sensitive: true }
   PASSDWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
