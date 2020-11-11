96126875f49414d837d876006b77f208d5b70625
tmynoy <a6.n_h.x@icloud.com>
2020-11-11 16:49:32 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+    authenticate_user!
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :category_id, :status_id, :price, :payment_id, :area_id, :send_day_id, :explanation, :image).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,27 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :payment
+  belongs_to :send_day
+  belongs_to :status
+
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    validates :image
+    validates :explanation, length: { maximum: 1000 }
+    PRICE_REGEX = /\A[0-9]+\z/
+    validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999, :message => 'is invalid'}, format: { with:PRICE_REGEX }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :area_id
+    validates :category_id
+    validates :payment_id
+    validates :send_day_id
+    validates :status_id
+  end
+end
