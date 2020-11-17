8b40d617272f4d390371ab654111af827d842b19
tmynoy <a6.n_h.x@icloud.com>
2020-11-17 17:08:00 +0900

    カード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,33 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
-      redirect action: :index
+      return redirect_to root_path
     else
       render action: :index
     end
   end
-
+  
   private
   def order_params
-    params.require(:order_address).permit(:postal, :area_id, :municipalities, :building_name, :block, :tell_num).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:postal, :area_id, :municipalities, :building_name, :block, :tell_num).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,22 +1,20 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal, :area_id, :municipalities, :building_name, :block, :tell_num
+  attr_accessor :postal, :area_id, :municipalities, :building_name, :block, :tell_num, :item_id, :token, :user_id
 
-  # バリデーション入力
+
   with_options presence: true do
     validates :postal, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :municipalities
     validates :block
     validates :tell_num, format: {with: /\A\d{10,11}\z/ }
-    validates :postal
+    validates :token
   end
   validates :area_id, numericality: { other_than: 1 }
-  
-  # バリデーション入力
+
 
   def save
-    user = User.find(current_user.id)
-    Order.create(user_id: current_user.id, item_id: item.id)
-    Address.create(:postal, :area_id, :municipalities, :building_name, :block, :tell_num, order_id: order.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal: postal, area_id: area_id, municipalities: municipalities, building_name: building_name, block: block, tell_num: tell_num, order_id: order.id)
   end
 end
